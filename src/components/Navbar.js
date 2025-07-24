67a2943af99c15f8f7dab30b4e96bcf70e21a474
Abhishek Yadav <152941703+Abhi25092@users.noreply.github.com>
2025-07-25 01:21:47 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
-import { MdDriveFolderUpload } from 'react-icons/md';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
+
     return(
         <nav className='navbar'>
-            <h1>My Portfolio</h1>
-            <ul>
-                <li><Link to="home" smooth={true} duration={600}>Home</Link></li>
-                <li><Link to="about" smooth={true} duration={600}>About</Link></li>
-                <li><Link to="projects" smooth={true} duration={600}>Projects</Link></li>
-                <li><Link to="skills" smooth={true} duration={600}>Skills</Link></li>
-                <li><Link to="contact" smooth={true} duration={600}>Contact</Link></li>
+            <div className='logo'>MyPortfolio</div>
+            <div className='hamburger' onClick={toggleMenu}>
+                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+                <div className={`bar ${isOpen ? 'open' : ''}`}></div>
+            </div>
+
+            <ul className={`nav-links ${isOpen ? 'active' : ''}`}>
+                <li><Link to="home" smooth={true} duration={500} onClick={closeMenu}>Home</Link></li>
+                <li><Link to="about" smooth={true} duration={500} onClick={closeMenu}>About</Link></li>
+                <li><Link to="projects" smooth={true} duration={500} onClick={closeMenu}>Projects</Link></li>
+                <li><Link to="skills" smooth={true} duration={500} onClick={closeMenu}>Skills</Link></li>
+                <li><Link to="contact" smooth={true} duration={500} onClick={closeMenu}>Contact</Link></li>
             </ul>
         </nav>
     );
